d97ae6b2712d1914caaf5b954e1482aaaee2671d
goncalhurtado <gonzalomilano@gmail.com>
2023-10-29 22:19:11 -0300

    added color pick input

--- src/components/Qr.jsx
+++ src/components/Qr.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import QRCode from "react-qr-code";
+import { MuiColorInput } from "mui-color-input";
 
 const Qr = ({ inputData }) => {
+  const [bg, setBg] = useState("#ffffff");
+  const [color, setColor] = useState("#000000");
+
   const data = inputData || "https://www.google.com";
 
+  const handlebg = (newValue) => {
+    setBg(newValue);
+  };
+  const handlecolor = (newValue) => {
+    setColor(newValue);
+  };
   return (
     <>
       <div>Qr</div>
-      <QRCode value={data} />
+      <QRCode value={data} bgColor={bg} fgColor={color} />
+      <MuiColorInput value={bg} onChange={handlebg} />
+      <MuiColorInput value={color} onChange={handlecolor} />
     </>
   );
 };
